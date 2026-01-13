c7da62ceccd502b2a976afc43ec93ba75e0bd1e5
Léa <la239239@student.helha.be>
2026-01-13 23:02:32 +0100

    feat(front): ajout formulaire création utilisateur et service HTTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideForms } from '@angular/forms';
 import { routes } from './app.routes';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(), // Active HttpClient pour les appels API
+    provideForms()       // Active les formulaires template-driven (ngModel)
   ]
 };
